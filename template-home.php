44d2e0e81c00e0283956c5c28b20bab9392af931
mudit jain <1996mudit1502@gmail.com>
2018-02-04 00:13:51 +0530

    added blank theme files

---
+++ template-home.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: homepage
+ */
+
+get_header();
+?>
+    <section id="primary" class="content-area">
+      homepage
+        <main id="main" class="site-main" role="main">
+            <?php
+                while ( have_posts() ) : the_post();
+                    get_template_part( 'template-parts/content', 'notitle' );
+                endwhile; // End of the loop.
+            ?>
+        </main><!-- #main -->
+    </section><!-- #primary -->
+
+<?php
+get_footer();
