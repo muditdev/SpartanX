391693b5982abf3638356cfd3edc633db6b0a170
mudit jain <1996mudit1502@lmsin.com>
2018-02-04 23:18:28 +0530

    home templete design code blocks styling

--- template-home.php
+++ template-home.php
@@ -6,14 +6,228 @@
 get_header();
 ?>
     <section id="primary" class="content-area">
-      homepage
-        <main id="main" class="site-main" role="main">
-            <?php
-                while ( have_posts() ) : the_post();
-                    get_template_part( 'template-parts/content', 'notitle' );
-                endwhile; // End of the loop.
-            ?>
-        </main><!-- #main -->
+      <div class="head-banner">
+        <div class="head-info-widget">
+          <ul>
+            <li>
+              <h3>250</h3>
+              <p>attendees</p>
+            </li>
+            <li>
+              <h3>30</h3>
+              <p>companies</p>
+            </li>
+            <li>
+              <h3>6</h3>
+              <p>speaker</p>
+            </li>
+            <li>
+              <h3>1</h3>
+              <p>goal</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <main id="main" class="site-main" role="main">
+        <section class="home-intro text-center">
+          <div class="container">
+            <h2 class="title">SPARTAN X <br> LEADERSHIP TALKS</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </section>
+        <section class="home-location">
+          <div class="container">
+            <h2 class="title text-center">2018 LOCATIONS</h2>
+            <div class="locations-container">
+              <div class="row">
+                <div class="col-md-4 col-sm-6">
+                  <div class="location">
+                    <div class="loc-bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/images/location.jpg)"></div>
+                    <div class="loc-hover">
+                      <h3 class="loc-title">NEW YORK</h3>
+                      <span class="loc-date">February 1, 2018</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                  <div class="location">
+                    <div class="loc-bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/images/location.jpg)"></div>
+                    <div class="loc-hover">
+                      <h3 class="loc-title">NEW YORK</h3>
+                      <span class="loc-date">February 1, 2018</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                  <div class="location">
+                    <div class="loc-bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/images/location.jpg)"></div>
+                    <div class="loc-hover">
+                      <h3 class="loc-title">NEW YORK</h3>
+                      <span class="loc-date">February 1, 2018</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                  <div class="location">
+                    <div class="loc-bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/images/location.jpg)"></div>
+                    <div class="loc-hover">
+                      <h3 class="loc-title">NEW YORK</h3>
+                      <span class="loc-date">February 1, 2018</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                  <div class="location">
+                    <div class="loc-bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/images/location.jpg)"></div>
+                    <div class="loc-hover">
+                      <h3 class="loc-title">NEW YORK</h3>
+                      <span class="loc-date">February 1, 2018</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                  <div class="location">
+                    <div class="loc-bg" style="background-image: url(<?php echo get_template_directory_uri() ?>/images/location.jpg)"></div>
+                    <div class="loc-hover">
+                      <h3 class="loc-title">NEW YORK</h3>
+                      <span class="loc-date">February 1, 2018</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="home-quote">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6">
+                <h3 class="q--title">
+                  "Life Changing <br> Event"
+                </h3>
+                <p class="q--author">
+                  <span class="name">John Smith</span>
+                  <span class="pos">CEO, Company Name</span>
+                </p>
+              </div>
+              <div class="col-md-6">
+              <p class="q--des">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="home-sponsors">
+          <div class="container">
+            <h2 class="title text-center">SPONSORS</h2>
+            <div class="sponsors">
+              <a href="javascript:void(0)">
+                <img src="<?php echo get_template_directory_uri() ?>/images/sponsor1.jpg" alt="">
+              </a>
+              <a href="javascript:void(0)">
+                <img src="<?php echo get_template_directory_uri() ?>/images/sponsor2.jpg" alt="">
+              </a>
+              <a href="javascript:void(0)">
+                <img src="<?php echo get_template_directory_uri() ?>/images/sponsor3.jpg" alt="">
+              </a>
+            </div>
+          </div>
+
+        </section>
+        <section class="home-faq">
+          <div class="container">
+            <h2 class="title text-center">FAQ</h2>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="faq-block">
+                  <h4 class="f--ques">
+                    What is Spartan X ?
+                  </h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                  </p>
+                </div>
+                <div class="faq-block">
+                  <h4 class="f--ques">
+                    What is Spartan X ?
+                  </h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                  </p>
+                </div>
+                <div class="faq-block">
+                  <h4 class="f--ques">
+                    What is Spartan X ?
+                  </h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="faq-block">
+                  <h4 class="f--ques">
+                    What is Spartan X ?
+                  </h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                  </p>
+                </div>
+                <div class="faq-block">
+                  <h4 class="f--ques">
+                    What is Spartan X ?
+                  </h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                  </p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+        <section class="home-tlf">
+          <h2 class="title text-center uc"> Training <br> leaders from : </h2>
+          <div class="sponsors">
+            <a href="javascript:void(0)">
+              <img src="<?php echo get_template_directory_uri() ?>/images/sponsor1.jpg" alt="">
+            </a>
+            <a href="javascript:void(0)">
+              <img src="<?php echo get_template_directory_uri() ?>/images/sponsor2.jpg" alt="">
+            </a>
+            <a href="javascript:void(0)">
+              <img src="<?php echo get_template_directory_uri() ?>/images/sponsor3.jpg" alt="">
+            </a>
+          </div>
+          <div class="sponsors">
+            <a href="javascript:void(0)">
+              <img src="<?php echo get_template_directory_uri() ?>/images/sponsor4.jpg" alt="">
+            </a>
+            <a href="javascript:void(0)">
+              <img src="<?php echo get_template_directory_uri() ?>/images/sponsor5.jpg" alt="">
+            </a>
+            <a href="javascript:void(0)">
+              <img src="<?php echo get_template_directory_uri() ?>/images/sponsor6.jpg" alt="">
+            </a>
+          </div>
+        </section>
+        <section class="home-cta">
+          <div class="container">
+            <h2 class="title uc text-center">
+              10 days left to reserve your spot
+            </h2>
+          </div>
+          <button type="button" class="btn buy-tickets" data-left="50">
+            Buy tickets
+          </button>
+        </section>
+          <?php
+              while ( have_posts() ) : the_post();
+                  get_template_part( 'template-parts/content', 'notitle' );
+              endwhile; // End of the loop.
+          ?>
+      </main><!-- #main -->
     </section><!-- #primary -->
 
 <?php
