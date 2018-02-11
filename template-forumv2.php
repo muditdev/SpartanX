1a85e48156c45ba1ebbedb9e80fc65960f0297c5
mudit jain <1996mudit1502@gmail.com>
2018-02-11 21:29:05 +0530

    updated forum1 forum2 template

---
+++ template-forumv2.php
@@ -0,0 +1,403 @@
+<?php
+/**
+ * Template Name: Forum2
+ */
+
+get_header();
+?>
+<section id="primary" class="content-area">
+  <div class="forum2-header">
+    <div class="f_2-content">
+      <div class="row align-items-center">
+        <div class="col-md-4 bannercont-left">
+          <h2>SPARTAN <span> X  </span> LEADERSHIP FORUM</h2>
+        </div>
+        <div class="col-md-8 bannercont-right">
+          <h1>Citizen’s Bank Park <br> Philadelphia, PA</h1>
+          <h4>September 22, 2018</h4>
+          <a href="javascript:void(0)" class="btn btn-dark">
+            REQUEST A SEAT
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <main id="main" class="site-main oxhide" role="main">
+    <section class="f_2-speakers">
+      <h1 class="title uc text-center">
+        speakers
+      </h1>
+      <div class="container container800">
+        <div class="row">
+          <div class="col-md-4 col-sm-6">
+            <div class="speaker">
+              <div class="speaker-img">
+                <img src="<?php echo get_template_directory_uri() ?>/images/speaker.jpg" alt="">
+              </div>
+              <div class="speaker-intro">
+                <span class="speaker-name">John Smith</span>
+                <span class="speaker-title"> CEO, Company Name</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="speaker">
+              <div class="speaker-img">
+                <img src="<?php echo get_template_directory_uri() ?>/images/speaker.jpg" alt="">
+              </div>
+              <div class="speaker-intro">
+                <span class="speaker-name">John Smith</span>
+                <span class="speaker-title"> CEO, Company Name</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="speaker">
+              <div class="speaker-img">
+                <img src="<?php echo get_template_directory_uri() ?>/images/speaker.jpg" alt="">
+              </div>
+              <div class="speaker-intro">
+                <span class="speaker-name">John Smith</span>
+                <span class="speaker-title"> CEO, Company Name</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="speaker">
+              <div class="speaker-img">
+                <img src="<?php echo get_template_directory_uri() ?>/images/speaker.jpg" alt="">
+              </div>
+              <div class="speaker-intro">
+                <span class="speaker-name">John Smith</span>
+                <span class="speaker-title"> CEO, Company Name</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="speaker">
+              <div class="speaker-img">
+                <img src="<?php echo get_template_directory_uri() ?>/images/speaker.jpg" alt="">
+              </div>
+              <div class="speaker-intro">
+                <span class="speaker-name">John Smith</span>
+                <span class="speaker-title"> CEO, Company Name</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="speaker">
+              <div class="speaker-img">
+                <img src="<?php echo get_template_directory_uri() ?>/images/speaker.jpg" alt="">
+              </div>
+              <div class="speaker-intro">
+                <span class="speaker-name">John Smith</span>
+                <span class="speaker-title"> CEO, Company Name</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="f-agenda secbg-gray">
+      <h2 class="title text-center">AGENDA</h2>
+      <div class="container">
+        <div class="agenda-wrap row">
+            <div class="agenda col-sm-4">
+              <i class="ico-chat"></i>
+              <h4>Discussion</h4>
+              <p>We created Spartan X Leadership Talks to foster an environment where leaders can meet, learn and execute the age-old principles.</p>
+            </div>
+            <div class="agenda col-sm-4">
+              <i class="ico-medal"></i>
+              <h4>Excercise</h4>
+              <p>We created Spartan X Leadership Talks to foster an environment where leaders can meet, learn and execute the age-old principles.</p>
+            </div>
+            <div class="agenda col-sm-4">
+              <i class="ico-eye"></i>
+              <h4>Obstacles</h4>
+              <p>We created Spartan X Leadership Talks to foster an environment where leaders can meet, learn and execute the age-old principles.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="f_2-bg-sap"></div>
+    <section class="f_2-pricing">
+      <h2 class="title text-center">SAVE YOUR SEAT</h2>
+      <div class="sp-pricing container container800">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="price-block active">
+              <h3 class="pb-title">Early Bird</h3>
+              <h4 class="pb-price">$800</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="price-block">
+              <h3 class="pb-title">Early Bird</h3>
+              <h4 class="pb-price">$800</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="price-block">
+              <h3 class="pb-title">Early Bird</h3>
+              <h4 class="pb-price">$800</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="f-mission text-center">
+      <div class="container container800">
+        <h1 class="title">OUR MISSION</h1>
+        <h3 class="subheading">Change 100 Million Lives</h3>
+        <p>We created Spartan X Leadership Talks to foster an environment where leaders can meet, learn and execute the age-old principles that instill betterment in corporate culture. Our goal is to help you become a better leader in your career and in your personal life.</p>
+        <img src="<?php echo get_template_directory_uri() ?>/images/mission-map.jpg" class="img-fluid" alt="">
+      </div>
+    </section>
+    <section class="home-tlf city-tlf f-tlf">
+      <div class="container">
+        <h2 class="title uc text-center"> Training leaders from : </h2>
+        <div class="sponsors">
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/applelogo.png" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/citylogo.png" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/comcast.png" alt="">
+          </a>
+        </div>
+        <div class="sponsors">
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/comcast.png" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/applelogo.png" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/citylogo.png" alt="">
+          </a>
+
+        </div>
+
+      </div>
+    </section>
+    <section class="f-intro mt30">
+      <div class="container container800">
+        <h1 class="title">BUILD A SHARPER MIND, <br>
+          STRONGER HEART, AND <br>
+          BETTER CULTURE</h1>
+          <div class="row">
+            <div class="col-sm-6">
+              <h3 class="subheading">
+                Improve as a leader, spouse, parent, and citizen today.
+              </h3>
+              <p>Spartan X Leadership Talks bring together inspiring speakers from around the world to share their knowledge about the 10 Spartan Principles for a Better Life. Each Leadership Talk focuses around one of the 10 principles. Spartan Masters will help you learn how to handle unforeseen obstacles, solve problems with wisdom and urgency, and maintain focus amid chaos and drama. </p>
+            </div>
+            <div class="col-sm-6">
+              <p>After our speakers present, we’ll test the skills you learned against real-life Spartan obstacles which will better equip you to perform at your best in the workplace and in life. Of course, there will be plenty of time to network over healthy snacks and learn from some of the brightest minds in business. </p>
+              <p>The best way to become a better leader is to start with yourself. With a healthy mind and body, and the right skills to lead, you can create a better culture at your company and lead your people together. </p>
+            </div>
+          </div>
+      </div>
+    </section>
+    <section class="f-wsa">
+      <div class="container container800">
+        <h1 class="title">WHO SHOULD ATTTEND ?</h1>
+        <div class="row">
+          <div class="col-sm-6">
+            <p>The SpartanX Leadership Forums are for business leaders looking to hone their skills and make an impact on the world. Our fast-paced, engaging format will not only help you improve as a leader in the workplace, but it will help you become a better spouse, parent, and citizen. </p>
+            <h3 class="subheading">If you’re looking to:</h3>
+            <ul>
+              <li>Find your True North</li>
+              <li>Make a commitment</li>
+              <li>Fuel your enthusiasm</li>
+              <li>Delay gratification</li>
+            </ul>
+          </div>
+          <div class="col-sm-6">
+            <ul>
+              <li>Maximize your time</li>
+              <li>Get gritty</li>
+              <li>Face adversity</li>
+              <li>Adjust your frame of reference</li>
+              <li>Be honorable</li>
+              <li>Be Spartan</li>
+            </ul>
+            <h3 class="subheading">Then SpartanX Leadership Forums are for you! </h3>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <section class="home-quote f-testimonials">
+      <div class="slider">
+        <div id="testimonalSlider" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <!-- <ul class="carousel-indicators">
+          <li data-target="#testimonalSlider" data-slide-to="0" class="active"></li>
+          <li data-target="#testimonalSlider" data-slide-to="1"></li>
+          <li data-target="#testimonalSlider" data-slide-to="2"></li>
+        </ul> -->
+        <!-- The slideshow -->
+        <div class="carousel-inner container">
+          <div class="carousel-item active">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="q--author">
+                  <figure>
+                    <img src="<?php echo get_template_directory_uri() ?>/images/author.jpg" alt="">
+                  </figure>
+                  <span class="name">John Smith</span>
+                  <span class="pos">CEO, Company Name</span>
+                </div>
+              </div>
+              <div class="col-md-8">
+                <h3 class="q--title">
+                  "Life Changing <br> Event"
+                </h3>
+                <p class="q--des">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="q--author">
+                  <figure>
+                    <img src="<?php echo get_template_directory_uri() ?>/images/author.jpg" alt="">
+                  </figure>
+                  <span class="name">John Smith</span>
+                  <span class="pos">CEO, Company Name</span>
+                </div>
+              </div>
+              <div class="col-md-8">
+                <h3 class="q--title">
+                  "Life Changing <br> Event"
+                </h3>
+                <p class="q--des">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="q--author">
+                  <figure>
+                    <img src="<?php echo get_template_directory_uri() ?>/images/author.jpg" alt="">
+                  </figure>
+                  <span class="name">John Smith</span>
+                  <span class="pos">CEO, Company Name</span>
+                </div>
+              </div>
+              <div class="col-md-8">
+                <h3 class="q--title">
+                  "Life Changing <br> Event"
+                </h3>
+                <p class="q--des">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#testimonalSlider" data-slide="prev">
+          <!-- <span class="carousel-control-prev-icon"></span> -->
+          <i class="ico-left"></i>
+        </a>
+        <a class="carousel-control-next" href="#testimonalSlider" data-slide="next">
+          <!-- <span class="carousel-control-next-icon"></span> -->
+          <i class="ico-right"></i>
+        </a>
+        </div>
+      </div>
+    </section>
+    <section class="home-sponsors">
+      <div class="container">
+        <h2 class="title text-center">SPONSORS</h2>
+        <div class="sponsors">
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/sponsor1.jpg" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/sponsor2.jpg" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/sponsor3.jpg" alt="">
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="home-faq">
+      <div class="container">
+        <h2 class="title text-center">FAQ</h2>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="faq-block">
+              <h4 class="f--ques">
+              Question 1
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+              </p>
+            </div>
+            <div class="faq-block">
+              <h4 class="f--ques">
+                Question 2
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+              </p>
+            </div>
+            <div class="faq-block">
+              <h4 class="f--ques">
+              Question 3
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="faq-block">
+              <h4 class="f--ques">
+              Question 4
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+              </p>
+            </div>
+            <div class="faq-block">
+              <h4 class="f--ques">
+              Question 5
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+      <?php /*
+          while ( have_posts() ) : the_post();
+              get_template_part( 'template-parts/content', 'notitle' );
+          endwhile; // End of the loop.
+    */  ?>
+  </main><!-- #main -->
+</section><!-- #primary -->
+<?php
+get_footer();
