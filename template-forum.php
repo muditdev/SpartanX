3e2e86203bb745ebfebe025f7081e1280c6e8bf7
mudit jain <1996mudit1502@gmail.com>
2018-02-11 10:18:52 +0530

    updated forum page sections

--- template-forum.php
+++ template-forum.php
@@ -24,12 +24,196 @@ get_header();
             </div>
           </div>
         </div>
-
     </div>
     </div>
   </div>
   <main id="main" class="site-main oxhide" role="main">
+    <section class="f-intro mt30">
+      <div class="container container800">
+        <h1 class="title">BUILD A SHARPER MIND, <br>
+          STRONGER HEART, AND <br>
+          BETTER CULTURE</h1>
+          <div class="row">
+            <div class="col-sm-6">
+              <h3 class="subheading">
+                Improve as a leader, spouse, parent, and citizen today.
+              </h3>
+              <p>Spartan X Leadership Talks bring together inspiring speakers from around the world to share their knowledge about the 10 Spartan Principles for a Better Life. Each Leadership Talk focuses around one of the 10 principles. Spartan Masters will help you learn how to handle unforeseen obstacles, solve problems with wisdom and urgency, and maintain focus amid chaos and drama. </p>
+            </div>
+            <div class="col-sm-6">
+              <p>After our speakers present, we’ll test the skills you learned against real-life Spartan obstacles which will better equip you to perform at your best in the workplace and in life. Of course, there will be plenty of time to network over healthy snacks and learn from some of the brightest minds in business. </p>
+              <p>The best way to become a better leader is to start with yourself. With a healthy mind and body, and the right skills to lead, you can create a better culture at your company and lead your people together. </p>
+            </div>
+          </div>
+      </div>
+    </section>
 
+    <section class="f-mission text-center secbg-gray">
+      <div class="container container800">
+        <h1 class="title">OUR MISSION</h1>
+        <h3 class="subheading">Change 100 Million Lives</h3>
+        <p>We created Spartan X Leadership Talks to foster an environment where leaders can meet, learn and execute the age-old principles that instill betterment in corporate culture. Our goal is to help you become a better leader in your career and in your personal life.</p>
+      </div>
+    </section>
+    <section class="f-eventloc">
+          <h1 class="title text-center">
+            2018 EVENT LOCATION
+          </h1>
+          <div class="container container800">
+            <div class="row">
+              <div class="col-md-4 col-sm-6">
+                <div class="speaker location">
+                  <div class="speaker-img loc-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/event-loc1.jpg" alt="">
+                  </div>
+                  <div class="speaker-hover loc-hover">
+                    <div class="text-center">
+                      <h2 class="uc">Arlington texas</h2>
+                      <a href="javascript:void(0)" class="btn btn-dark btn-block">VIEW DETAILS</a>
+                      <a href="javascript:void(0)" class="btn btn-light btn-block">REQUEST TICKETS</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-6">
+                <div class="speaker location">
+                  <div class="speaker-img loc-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/event-loc1.jpg" alt="">
+                  </div>
+                  <div class="speaker-hover loc-hover">
+                    <div class="text-center">
+                      <h2 class="uc">Arlington texas</h2>
+                      <a href="javascript:void(0)" class="btn btn-dark btn-block">VIEW DETAILS</a>
+                      <a href="javascript:void(0)" class="btn btn-light btn-block">REQUEST TICKETS</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-6">
+                <div class="speaker location">
+                  <div class="speaker-img loc-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/event-loc1.jpg" alt="">
+                  </div>
+                  <div class="speaker-hover loc-hover">
+                    <div class="text-center">
+                      <h2 class="uc">Arlington texas</h2>
+                      <a href="javascript:void(0)" class="btn btn-dark btn-block">VIEW DETAILS</a>
+                      <a href="javascript:void(0)" class="btn btn-light btn-block">REQUEST TICKETS</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-6">
+                <div class="speaker location">
+                  <div class="speaker-img loc-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/event-loc1.jpg" alt="">
+                  </div>
+                  <div class="speaker-hover loc-hover">
+                    <div class="text-center">
+                      <h2 class="uc">Arlington texas</h2>
+                      <a href="javascript:void(0)" class="btn btn-dark btn-block">VIEW DETAILS</a>
+                      <a href="javascript:void(0)" class="btn btn-light btn-block">REQUEST TICKETS</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-6">
+                <div class="speaker location">
+                  <div class="speaker-img loc-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/event-loc1.jpg" alt="">
+                  </div>
+                  <div class="speaker-hover loc-hover">
+                    <div class="text-center">
+                      <h2 class="uc">Arlington texas</h2>
+                      <a href="javascript:void(0)" class="btn btn-dark btn-block">VIEW DETAILS</a>
+                      <a href="javascript:void(0)" class="btn btn-light btn-block">REQUEST TICKETS</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-6">
+                <div class="speaker location">
+                  <div class="speaker-img loc-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/event-loc1.jpg" alt="">
+                  </div>
+                  <div class="speaker-hover loc-hover">
+                    <div class="text-center">
+                      <h2 class="uc">Arlington texas</h2>
+                      <a href="javascript:void(0)" class="btn btn-dark btn-block">VIEW DETAILS</a>
+                      <a href="javascript:void(0)" class="btn btn-light btn-block">REQUEST TICKETS</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    </section>
+    <section class="home-tlf city-tlf">
+      <div class="container">
+        <h2 class="title uc text-center"> Training leaders from : </h2>
+        <div class="sponsors">
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/sponsor1.jpg" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/sponsor2.jpg" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/sponsor3.jpg" alt="">
+          </a>
+        </div>
+        <div class="sponsors">
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/sponsor4.jpg" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/sponsor5.jpg" alt="">
+          </a>
+          <a href="javascript:void(0)">
+            <img src="<?php echo get_template_directory_uri() ?>/images/sponsor6.jpg" alt="">
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="f-wsa">
+      <div class="container container800">
+        <h1 class="title">WHO SHOULD ATTTEND ?</h1>
+        <div class="row">
+          <div class="col-sm-6">
+            <p>The SpartanX Leadership Forums are for business leaders looking to hone their skills and make an impact on the world. Our fast-paced, engaging format will not only help you improve as a leader in the workplace, but it will help you become a better spouse, parent, and citizen. </p>
+            <h3 class="subheading">If you’re looking to:</h3>
+            <ul>
+              <li>Find your True North</li>
+              <li>Make a commitment</li>
+              <li>Fuel your enthusiasm</li>
+              <li>Delay gratification</li>
+            </ul>
+          </div>
+          <div class="col-sm-6">
+            <ul>
+              <li>Maximize your time</li>
+              <li>Get gritty</li>
+              <li>Face adversity</li>
+              <li>Adjust your frame of reference</li>
+              <li>Be honorable</li>
+              <li>Be Spartan</li>
+            </ul>
+            <h3 class="subheading">Then SpartanX Leadership Forums are for you! </h3>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <section class="f-benifits secbg-gray">
+      <div class="container">
+        <h1 class="title text-center">
+          BENEFITS OF ATTENDING
+        </h1>
+        <h3 class="subheading text-center">
+          When you attend a Spartan X <br>   Leadership Talk, you will:
+        </h3>
+       </div>
+    </section>
       <?php /*
           while ( have_posts() ) : the_post();
               get_template_part( 'template-parts/content', 'notitle' );
